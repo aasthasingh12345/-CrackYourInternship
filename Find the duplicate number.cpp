11226a6ff0b75009d3dbf4a6c39a86f9e638977d
aasthasingh12345 <93035322+aasthasingh12345@users.noreply.github.com>
2022-05-03 23:13:50 +0530

    Add files via upload

---
+++ Find the duplicate number.cpp
@@ -0,0 +1,34 @@
+//Arrays
+//Find the duplicate number
+#include<iostream>
+using namespace std;
+int find_dublicate(int arr[],int n)
+{
+	int x=0;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(arr[i]==arr[j])
+			{
+				return arr[i];
+			}
+			
+		}
+	}
+}
+int main()
+{
+	int n;
+	cout<<"Enter the number of elements in the array:";
+	cin>>n;
+	int arr[n];
+	cout<<"Enter the elements of the array:";
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	int y=find_dublicate(arr,n);
+	cout<<"The dublicate number is :"<<y;
+	return 0;
+}
